1c669707b0931cbe9d4b3cb5a39a6b050073703b
Alexis Dela Torre <alexisndelatorre@gmail.com>
2022-04-12 03:31:40 +0000

    deployment#2: debugging heroku h13

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -1,21 +1,21 @@
-const parse = require('pg-connection-string').parse;
-const pgconfig = parse(process.env.DATABASE_URL);
-pgconfig.ssl = {
-    rejectUnauthorized: false
-};
+// const parse = require('pg-connection-string').parse;
+// const pgconfig = parse(process.env.DATABASE_URL);
+// pgconfig.ssl = {
+//     rejectUnauthorized: false
+// };
 
 const knex = require('knex')({
     "client": process.env.DB_DRIVER,
-    "connection": pgconfig
-    // {
-    //     "user": process.env.DB_USER,
-    //     "password": process.env.DB_PASS,
-    //     "database": process.env.DB_DB,
-    //     "host": process.env.DB_HOST,
-    //     "ssl": {
-    //         'rejectUnauthorized': false
-    //     }
-    // }
+    "connection": {
+         "user": process.env.DB_USER,
+         "password": process.env.DB_PASS,
+         "database": process.env.DB_DB,
+         "host": process.env.DB_HOST,
+         "ssl": {
+            'rejectUnauthorized': false
+         }
+     }
+     // pgconfig
 });
 
 const bookshelf = require('bookshelf')(knex);
